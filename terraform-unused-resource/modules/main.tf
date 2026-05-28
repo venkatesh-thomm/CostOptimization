db7d4fd12532fbcaa9fbec29d1929bbf8704f2ea
Venkatesh Thommandru <venkatesh.thomm@gmail.com>
2026-05-28 08:32:44 +0530

    Updated

--- terraform-unused-resource/modules/main.tf
+++ terraform-unused-resource/modules/main.tf
@@ -61,24 +61,16 @@ locals {
 resource "aws_cloudwatch_metric_alarm" "ec2_idle" {
 
   for_each = toset(local.filtered_ec2_instances)
-
   alarm_name = "${var.alarm_prefix}-ec2-idle-${each.value}"
-
   comparison_operator = "LessThanOrEqualToThreshold"
-
   evaluation_periods  = var.evaluation_periods
   datapoints_to_alarm = var.evaluation_periods
-
   metric_name = "CPUUtilization"
   namespace   = "AWS/EC2"
-
   period    = var.alarm_period_seconds
   statistic = "Average"
-
   threshold = var.cpu_idle_threshold
-
   treat_missing_data = "notBreaching"
-
   alarm_description = "EC2 idle detection alarm"
 
   dimensions = {
@@ -101,19 +93,12 @@ resource "aws_cloudwatch_metric_alarm" "ec2_idle" {
 resource "aws_cloudwatch_metric_alarm" "unused_ebs" {
 
   for_each = toset(local.filtered_ebs_volumes)
-
   alarm_name = "${var.alarm_prefix}-unused-ebs-${each.value}"
-
   comparison_operator = "LessThanThreshold"
-
   evaluation_periods = var.evaluation_periods
-
   datapoints_to_alarm = var.evaluation_periods
-
   threshold = 1
-
   treat_missing_data = "breaching"
-
   alarm_description = "EBS volume has no read/write activity"
 
   alarm_actions = [
@@ -121,30 +106,19 @@ resource "aws_cloudwatch_metric_alarm" "unused_ebs" {
   ]
 
   metric_query {
-
     id = "total_ops"
-
     expression = "m1 + m2"
-
     label = "TotalIOPS"
-
     return_data = true
   }
 
   metric_query {
-
     id = "m1"
-
     metric {
-
       metric_name = "VolumeReadOps"
-
       namespace = "AWS/EBS"
-
       period = var.alarm_period_seconds
-
       stat = "Sum"
-
       dimensions = {
         VolumeId = each.value
       }
@@ -152,19 +126,12 @@ resource "aws_cloudwatch_metric_alarm" "unused_ebs" {
   }
 
   metric_query {
-
     id = "m2"
-
     metric {
-
       metric_name = "VolumeWriteOps"
-
       namespace = "AWS/EBS"
-
       period = var.alarm_period_seconds
-
       stat = "Sum"
-
       dimensions = {
         VolumeId = each.value
       }
